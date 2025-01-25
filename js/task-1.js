d742888097eb054cd41782553faedc1c35605889
Maksym <zzzeleznyakmaxim@gmail.com>
2025-01-25 20:59:01 +0200

    full

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,24 @@
-'use strict';
-
-function slugify(title) {
-  let slug = title.toLowerCase().split(' ');
-  return slug.join('-');
+function isEnoughCapacity(products, containerSize) {
+  let totalNumber = 0;
+  const values = Object.values(products);
+  for (const value of values) {
+    totalNumber += value;
+  }
+  return totalNumber <= containerSize;
 }
 
-console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
