5a2eae95552ae71f41affb4ddb477744d81d09cf
wftl1136 <113915919+wftl1136@users.noreply.github.com>
2025-01-25 20:56:24 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function filterArray(numbers, value) {
+  let commonNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      commonNumbers.push(numbers[i]);
+    }
+  }
+  return commonNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
